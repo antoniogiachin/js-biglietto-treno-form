e1492488517f845383a11ffa217b6ff7bb2a71f1
antoniogiachin <giachinantonio@outlook.com>
2022-01-25 10:00:51 +0100

    fine esercizio

--- main.js
+++ main.js
@@ -1,61 +1,65 @@
+// Mi riferisco a elementi DOM
 const name = document.getElementById('name');
 
 const km = document.getElementById('km');
 
 const age = document.getElementById('age');
 
-//Buttons
+// Buttons DOM
 const generate = document.getElementById('generate');
 
 const revert = document.getElementById('revert');
 
-// Elementi per inject
-const nameTicket = document.getElementById('nameTicket');
-const typeTicket = document.getElementById('typeTicket');
-const carriageTicket = document.getElementById('carriageTicket');
-const cpTicket = document.getElementById('cpTicket');
-const priceTicket = document.getElementById('priceTicket');
+// ticket output
+const ticketName = document.getElementById('ticketName');
+const ticketType = document.getElementById('ticketType');
+const ticketCarriage = document.getElementById('ticketCarriage');
+const ticketCp = document.getElementById('ticketCp');
+const ticketPrice = document.getElementById('ticketPrice');
 
-// Prezzi
-const price = km.value * 0.21;
-console.log(price);
+// Carrozza e Cp
+const carriage = Math.floor(Math.random() * 8 + 1);
 
-const overPrice = price - ((price * 40) / 100);
-console.log(overPrice);
+const cp = Math.floor(Math.random() * 9999 + 90000);
 
-const underPrice = price - ((price * 20) / 100);
-console.log(underPrice);
+// Mostra biglietto
+const showTicket = document.getElementById('showTicket');
 
-// cp e carriage
-let cp =  Math.floor(Math.random() * 9999 + 90000);
-
-let carriage = Math.floor(Math.random() * 9 + 1 );
-
-// Click buttons
+// azione button
 generate.addEventListener('click',
 
-    function() {
-
-        nameTicket.innerHTML = name.value;
-        carriageTicket.innerHTML = carriage;
-        cpTicket.innerHTML = cp;
-
-        if  (age.value == ""){
-            alert("Inserisci la tua eta'!");
+function(){
+        // Prezzo biglietto
+        const price = km.value * 0.21;
+        console.log(price);
+        
+        const overPrice = price - (price * 0.4);
+        console.log(overPrice);
+        
+        const underPrice = price - (price * 0.2);
+        console.log(underPrice);
+    
+        if(age.value == ""){
+            alert("Inserisci una età valida!");
+        } else if (age.value == "maggiorenni"){
+            ticketType.innerHTML = "Biglietto Standard";
+            ticketPrice.innerHTML = price.toFixed(2) + " &euro;";
+            showTicket.classList.remove('d-none');        
+        } else if (age.value == "over65"){
+            ticketType.innerHTML = "Biglietto Over 65";
+            ticketPrice.innerHTML = overPrice.toFixed(2) + " &euro;";
+            showTicket.classList.remove('d-none');        
         } else {
-            
-            if (age.value == "maggiorenne"){
-                typeTicket.innerHTML = "Biglietto Standard";
-                priceTicket.innerHTML = price.toFixed(2) + " &euro; ";
-            } else if (age.value == "over65"){
-                typeTicket.innerHTML = "Biglietto Over 65";
-                priceTicket.innerHTML = overPrice.toFixed(2) + " &euro; ";
-            }  else {
-                typeTicket.innerHTML = "Biglietto Minorenni";
-                priceTicket.innerHTML = underPrice.toFixed(2) + " &euro; ";
-            }
-            displayTicket.className =  "d-block " + "row" + " bg-wh " + " p-2";
+            ticketType.innerHTML = "Biglietto Minorenni";
+            ticketPrice.innerHTML = underPrice.toFixed(2) + " &euro;";
+            showTicket.classList.remove('d-none'); 
         }
+
+        ticketName.innerHTML = name.value;
+
+        ticketCarriage.innerHTML = carriage;
+
+        ticketCp.innerHTML = cp;    
 
     }
 
@@ -64,12 +68,15 @@ generate.addEventListener('click',
 revert.addEventListener('click',
 
     function(){
-        
-        displayTicket.className =  "d-none" ;
 
-        age.value="";
-        name.value="";
-        km.value="";
+        name.value ="";
+
+        km.value = "";
+
+        age.value ="";
+
+        showTicket.classList.add('d-none');        
+             
 
     }
 
